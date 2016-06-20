95d80376f0b2c6dd64a43cc7041cc8547f8525fb
David Prothero <david@prothero.com>
2016-06-20 15:50:51 -0700

    Initial commit

---
+++ TwilioCongressBot.Web/Controllers/LegistatorController.cs
@@ -0,0 +1,93 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Text.RegularExpressions;
+using System.Web.Mvc;
+using Twilio.Mvc;
+using Twilio.TwiML;
+using Twilio.TwiML.Mvc;
+using TwilioCongressBot.Web.Models;
+
+namespace TwilioCongressBot.Web.Controllers
+{
+  public class LegislatorController : Controller
+  {
+    private string _congress_api_url_template = "https://congress.api.sunlightfoundation.com/legislators/locate?zip={0}&apikey={1}";
+
+    private IDictionary<string, string> labelDict = new Dictionary<string, string>()
+    {
+      { "house", "House Representative" },
+      { "senate", "Senator" }
+    };
+
+    [HttpPost]
+    public ActionResult Index(SmsRequest request)
+    {
+      var response = new TwilioResponse();
+
+      var zip = getValidZip(request, response);
+      if(zip == null)
+      {
+        response.Message("Please text your ZIP code.");
+      }
+      else
+      {
+        var legislators = getLegislators(zip);
+        if (legislators == null)
+        {
+          response.Message("Error retrieving results for " + zip);
+        }
+        else
+        {
+          foreach(var legislator in legislators.results)
+          {
+            response.Message("Your " + labelDict[legislator.chamber] + ": " +
+              getFullName(legislator) + " - " + legislator.phone + " - " +
+              legislator.oc_email);
+          }
+        }
+      }
+
+      return new TwiMLResult(response);
+    }
+
+    private CongressApiResult getLegislators(string zip)
+    {
+      var url = string.Format(_congress_api_url_template, zip, ConfigurationManager.AppSettings["CongressApiKey"]);
+      var client = new RestClient(url);
+      var request = new RestRequest(Method.GET);
+      var response = client.Execute(request);
+      if (response.ResponseStatus != ResponseStatus.Completed || 
+        response.StatusCode != System.Net.HttpStatusCode.OK)
+      {
+        return null;
+      }
+      return JsonConvert.DeserializeObject<CongressApiResult>(response.Content);
+    }
+
+    private string getValidZip(SmsRequest request, TwilioResponse response)
+    {
+      if (isValidZip(request.Body)) return request.Body;
+      if (isValidZip(request.FromZip))
+      {
+        response.Message("Text your ZIP code for more accurate results. We're guessing " + request.FromZip);
+        return request.FromZip;
+      }
+      return null;
+    }
+
+    private Regex _zipValidator = new Regex(@"^\d{5}$");
+    private bool isValidZip(string zipToCheck)
+    {
+      return _zipValidator.IsMatch(zipToCheck);
+    }
+
+    private string getFullName(Legislator legislator)
+    {
+      return (legislator.first_name + " " +
+              legislator.middle_name + " " +
+              legislator.last_name).Replace("  ", " ");
+    }
+  }
+}
---
+++ TwilioCongressBot.Web/Models/CongressApiResult.cs
@@ -0,0 +1,57 @@
+﻿namespace TwilioCongressBot.Web.Models
+{
+  public class CongressApiResult
+  {
+    public Legislator[] results { get; set; }
+    public int count { get; set; }
+    public Page page { get; set; }
+  }
+
+  public class Page
+  {
+    public int count { get; set; }
+    public int per_page { get; set; }
+    public int page { get; set; }
+  }
+
+  public class Legislator
+  {
+    public string bioguide_id { get; set; }
+    public string birthday { get; set; }
+    public string chamber { get; set; }
+    public string contact_form { get; set; }
+    public string crp_id { get; set; }
+    public int? district { get; set; }
+    public string facebook_id { get; set; }
+    public string fax { get; set; }
+    public string[] fec_ids { get; set; }
+    public string first_name { get; set; }
+    public string gender { get; set; }
+    public string govtrack_id { get; set; }
+    public int icpsr_id { get; set; }
+    public bool in_office { get; set; }
+    public string last_name { get; set; }
+    public object middle_name { get; set; }
+    public object name_suffix { get; set; }
+    public object nickname { get; set; }
+    public string oc_email { get; set; }
+    public string ocd_id { get; set; }
+    public string office { get; set; }
+    public string party { get; set; }
+    public string phone { get; set; }
+    public string state { get; set; }
+    public string state_name { get; set; }
+    public string term_end { get; set; }
+    public string term_start { get; set; }
+    public string thomas_id { get; set; }
+    public string title { get; set; }
+    public string twitter_id { get; set; }
+    public int votesmart_id { get; set; }
+    public string website { get; set; }
+    public string youtube_id { get; set; }
+    public string lis_id { get; set; }
+    public int senate_class { get; set; }
+    public string state_rank { get; set; }
+  }
+
+}
